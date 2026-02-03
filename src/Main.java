f014755632685937f81a1fc9f40190e3a1ec8c7a
Rohankumar Naveen Patil <rp143014300@gmail.com>
2026-02-03 14:57:06 +0530

    Day 1: Java program structure and first run

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java & Git 🚀");
+        System.out.println("my name is rohankumar nmavven patil");
+        System.out.println("I an learning java");
+        System.out.println("this is my first code in java");
     }
 }
