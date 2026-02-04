72f59cb7f598a8f2bb49c3533b47228240eef627
Rohankumar Naveen Patil <rp143014300@gmail.com>
2026-02-04 14:22:41 +0530

    Day 2: Explained primitive and non-primitive data types

---
+++ src/day_2__DataType.java
@@ -0,0 +1,19 @@
+public class day_2__DataType {
+    public static void main(String[] args) {
+        int age = 22;
+        String name = "Rohankumar";
+        float height = 5.9f;
+        double weight = 70.5;
+        char blodGroup = 'A';
+        long phoneNumber = 8951589455L;
+        byte smallNumber = 100;
+        array 
+        System.out.println("Age: " + age);
+        System.out.println("Name: " + name);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+        System.out.println("Blood Group: " + blodGroup);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Small Number: " + smallNumber);
+    }
+}
